02e6c31ee3672dd48f606674d86de54f0e65619e
PatrickWienzek <Patrick.Wienzek@informatik.hs-fulda.de>
2017-01-16 20:40:43 +0100

    Physics
    Run Animation, Jump, Walljump, etc

--- js/states/play.js
+++ js/states/play.js
@@ -30,6 +30,9 @@ SpaceBear.playState.prototype = {
     this.map.setCollisionBetween(1, 2000, true, 'fragileLayer');
 
     this.noInteractionLayer.resizeWorld();
+
+    this.createFuel();
+
     //the camera will follow the player in the world
     this.game.camera.follow(this.player);
 
@@ -37,7 +40,12 @@ SpaceBear.playState.prototype = {
   update: function() {
 
     this.game.physics.arcade.collide(this.player, this.stageLayer);
-    this.game.physics.arcade.collide(this.player, this.trapsLayer);
+    this.game.physics.arcade.collide(this.player, this.trapsLayer,
+    this.playerTrapHandler, null, this);
+    this.game.physics.arcade.collide(this.player, this.fragileLayer,
+    this.playerFragileHandler, null, this);
+    this.game.physics.arcade.collide(this.player, this.fuels, 
+    this.playerFuelHandler, null, this);
 
     /*if (cursors.up.isDown)
     {
@@ -59,6 +67,93 @@ SpaceBear.playState.prototype = {
   }
 };
 
+SpaceBear.playState.prototype.playerFuelHandler = function(player, fuel) {
+  fuel.body.velocity.x = 0;
+  fuel.body.velocity.y = 0;
+  // flag to disable fuelHandler (multiple fuels)
+  if (this.transporting) {
+    return;
+  }
+  this.transporting = true;
+
+  // flag that the state is resetting because of a new level (for tut text)
+  //SpaceBear.newLevel = true;
+
+  // stop following player with camera
+  this.game.camera.unfollow();
+
+  // destroy player and fuel
+  fuel.pendingDestroy = true;
+  player.pendingDestroy = true;
+};
+
+SpaceBear.playState.prototype.playerTrapHandler = function(player, trap) {
+  //camera stops following player
+  this.game.camera.unfollow();
+  // player dies
+  player.pendingDestroy = true;
+
+  //Todecounter?
+
+  // shake camera
+  // this.startCameraShake();
+
+  // show some text, if not already showing any
+  if (!this.drawTutText) {
+    var text = '';
+    var rand = Math.random();
+    if (rand < 0.1) {
+      text = 'HAHAHAHA';
+    } else if (rand < 0.2) {
+      text = 'OUCHIE :[';
+    } else if (rand < 0.3) {
+      text = 'Try again T.T';
+    } else if (rand < 0.4){
+      text = 'You win! JK you died.';
+    } else if (rand < 0.5) {
+      text = '*burp*';
+    } else if (rand < 0.6) {
+      text = 'come on now :[';
+    } else if (rand < 0.7) {
+      text = 'What is... feeling?';
+    } else if (rand < 0.8) {
+      text = 'Juicy';
+    } else if (rand < 0.9) {
+      text = 'nice try, you got this <3';
+    } else {
+      text = 'You\'re breaking my <3';
+    }
+    this.deathText = this.game.add.bitmapText(this.game.camera.x + (this.game.camera.width / 2), this.game.camera.y + (this.game.camera.height / 2), 'carrier_command', text, 25);
+    this.deathText.anchor.setTo(0.5, 0.5);
+  }
+
+  // shake the camera
+  this.game.camera.shake(0.004, 1200);
+  this.game.camera.onShakeComplete.addOnce(function() {
+    // restart level after camera shake
+    this.game.camera.fade(0x000000, 250);
+    this.game.camera.onFadeComplete.addOnce(function() {
+      this.game.state.start(this.game.state.current);
+    }, this);
+  }, this);
+};
+
+SpaceBear.playState.prototype.playerFragileHandler = function(player, block) {
+  // block disappears after .25 seconds
+  this.game.time.events.add(250, function() {
+
+    // store block index so we can replace it later
+    var index = block.index;
+    this.map.removeTile(block.x, block.y, 'fragileLayer');
+    /*// replace block 2s after it disappears
+    this.game.time.events.add(2000, function() {
+     
+      // place the block
+      this.map.putTile(index, block.x, block.y, 'fragileLayer');
+    }, this);*/
+  }, this);
+};
+
 SpaceBear.playState.prototype.findObjectsByType = function(type, map, layer) {
 var result = new Array();
 
@@ -70,4 +165,28 @@ map.objects[layer].forEach(function(element){
   }
  });
  return result;
+};
+
+SpaceBear.playState.prototype.createFromTiledObject = function(element, group) {
+  var sprite = group.create(element.x, element.y, element.properties.sprite);
+  //copy all properties to the sprite
+  Object.keys(element.properties).forEach(function(key){
+    sprite[key] = element.properties[key];
+  });
+
+  // play animation
+  if (sprite.animated) {
+    sprite.animations.add('default');
+    sprite.animations.play('default', 10, true);
+  }
+};
+
+SpaceBear.playState.prototype.createFuel = function() {
+  // create end-of-level fuel
+  this.fuels = this.game.add.group();
+  this.fuels.enableBody = true;
+  var result = this.findObjectsByType('fuelz', this.map, 'objectsLayer');
+  result.forEach(function(element){
+    this.createFromTiledObject(element, this.fuels);
+  }, this);
 };
--- js/states/preload.js
+++ js/states/preload.js
@@ -6,8 +6,9 @@ SpaceBear.preloadState.prototype = {
 	preload: function() {
 		this.load.tilemap('map1', 'assets/tilemaps/1.json', null, Phaser.Tilemap.TILED_JSON);
 
-		this.load.image('player', 'assets/img/polarbear.png');
+		this.load.spritesheet('player', 'assets/img/player_sprite.png', 40, 40, 12);
 		this.load.image('marstiles', 'assets/img/marsLevelFloor.png');
+		this.load.spritesheet('fuel', 'assets/img/fuel.png', 40, 40);
 
 
 	},
